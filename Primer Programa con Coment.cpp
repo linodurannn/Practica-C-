b6ac0f3def3eff8962fb11bac51bb54c3d0f4387
linodurannn <155786462+linodurannn@users.noreply.github.com>
2024-01-10 21:13:14 -0400

    Add files via upload

---
+++ Primer Programa con Coment.cpp
@@ -0,0 +1,18 @@
+/*
+Titulo: Primer programa con comentarios 
+Autor: Juan David Meza Gonzales 
+Descripcion: Primer programa para analizar su estructura.
+Fecha: 2024-01-10
+*/
+
+#include <iostream> //Inclusion de la librearia de entrada y salida. 
+using namespace std; //El uso de espacio de nombre para STD de la I/O. 
+int main() //Funcion principal main para retornar un entero. 
+{ //Inicio
+	int numero; //Declaracion de la variable numero como entero. 
+	
+	numero = 5 + 5; //Asignacion de la suma de 5+5 a la varaible numero.
+	//Impresion del resultado de la variable numero 
+	cout <<"El resultado de la suma es:"<<numero; 
+	return 0; //Retorno de 0 para finalizar el programa. 
+} //Cierre 
