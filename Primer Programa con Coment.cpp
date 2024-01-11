d555c469a911858dd50a58429e15ccbe48334310
linodurannn <155786462+linodurannn@users.noreply.github.com>
2024-01-10 21:26:07 -0400

    Primer Programa con Coment.cpp

--- Primer Programa con Coment.cpp
+++ Primer Programa con Coment.cpp
@@ -5,13 +5,13 @@ Descripcion: Primer programa para analizar su estructura.
 Fecha: 2024-01-10
 */
 
-#include <iostream> //Inclusion de la librearia de entrada y salida. 
+#include <iostream> //Inclusion de la libreria de entrada y salida. 
 using namespace std; //El uso de espacio de nombre para STD de la I/O. 
 int main() //Funcion principal main para retornar un entero. 
 { //Inicio
 	int numero; //Declaracion de la variable numero como entero. 
 	
-	numero = 5 + 5; //Asignacion de la suma de 5+5 a la varaible numero.
+	numero = 5 + 5; //Asignacion de la suma de 5+5 a la variable numero.
 	//Impresion del resultado de la variable numero 
 	cout <<"El resultado de la suma es:"<<numero; 
 	return 0; //Retorno de 0 para finalizar el programa. 
